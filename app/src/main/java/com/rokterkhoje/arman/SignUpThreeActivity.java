90cbfff95aed3d71f2bbd0c809885c50a84fc8c4
Sb-Rahman <sb.rahman@yahoo.com>
2022-12-19 16:32:11 +0600

    custom blood donation apps

--- app/src/main/java/com/rokterkhoje/arman/SignUpThreeActivity.java
+++ app/src/main/java/com/rokterkhoje/arman/SignUpThreeActivity.java
@@ -2,9 +2,21 @@ package com.rokterkhoje.arman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class SignUpThreeActivity extends AppCompatActivity {
+
+    //Declaring gloobal variables
+    EditText lastDonate;
+    DatePickerDialog datePickerDialog;
+    DatePickerDialog.OnDateSetListener onDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +25,41 @@ public class SignUpThreeActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+
+
+
+        //initialization of variables
+        lastDonate = findViewById(R.id.lastdonatecalender);
+
+
+        lastDonate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //creating Calender instance and geting date ,month, year
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                //datepicker dialoage
+                datePickerDialog = new DatePickerDialog(getApplicationContext(), onDateSetListener, year, month, day);
+                datePickerDialog.show();
+
+
+            }
+        });
+
+        onDateSetListener =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                month=month+1;
+                lastDonate.setText(dayOfMonth+"/"+month+"/"+year);
+            }
+        };
+
+
     }
+
 }
--- app/src/main/java/com/rokterkhoje/arman/SignUpTwoActivity.java
+++ app/src/main/java/com/rokterkhoje/arman/SignUpTwoActivity.java
@@ -2,6 +2,7 @@ package com.rokterkhoje.arman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -399,6 +400,15 @@ public class SignUpTwoActivity extends AppCompatActivity {
             }
         });
 
+
+
+   buttonNext.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           Intent intent=new Intent(SignUpTwoActivity.this,SignUpThreeActivity.class);
+           startActivity(intent);
+       }
+   });
     }
 
     private void initialization() {
