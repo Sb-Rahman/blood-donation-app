71e04ba9f418dd8e685e5b3f160ca38260314c7b
Sb-Rahman <sb.rahman@yahoo.com>
2022-12-21 02:06:17 +0600

    custom blood donation apps

---
+++ app/src/main/java/com/rokterkhoje/arman/AboutApps.java
@@ -0,0 +1,14 @@
+package com.rokterkhoje.arman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutApps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_apps);
+    }
+}
---
+++ app/src/main/java/com/rokterkhoje/arman/PostRequest.java
@@ -0,0 +1,14 @@
+package com.rokterkhoje.arman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_request);
+    }
+}
